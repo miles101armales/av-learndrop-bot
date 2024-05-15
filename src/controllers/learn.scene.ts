0e65798eda801b4b7b7b709263c6a6f24028fef3
Arman Kuzembaev <armansk8er@gmail.com>
2024-05-15 22:41:12 +0500

    final version with test video

--- src/controllers/learn.scene.ts
+++ src/controllers/learn.scene.ts
@@ -1,6 +1,7 @@
 import { IBotContext } from '../models/context.interface';
 import { Scene } from '../models/scene.class';
 import { Composer, Scenes, Telegraf } from 'telegraf';
+import { final, greeting, video0_5000, video10000, video5000_10000 } from './constants';
 
 export class LearnScene extends Scene {
 	state: string;
@@ -9,88 +10,56 @@ export class LearnScene extends Scene {
 		super(bot);
 	}
 	handle(): void {
-		const stepHandlerforVideo = new Composer<IBotContext>();
-		stepHandlerforVideo.action('0100', ctx => {
-			ctx.reply('Загрузка...');
-			ctx.telegram.sendVideo(ctx.chat?.id, { source: './src/public/video/video1.MP4' }, {
-				reply_markup: {
-					inline_keyboard: [[{text: 'ПРОСМОТРЕНО✅', callback_data: 'watch1_complete'}]]
-				},
-				width: 720,
-				height: 1280,
-				caption: '☝️ Посмотрите скорее видео, Виталий простым языком расскажет:' +
-				'\n\n1️⃣ Секрет, который не вошел в эфир' +
-				'\n2️⃣ Какие проекты вы можете использовать, чтобы заработать на дропах (инфа из платного курса!)' +
-				'\n3️⃣ Как прямо сейчас получить инструкцию напрямую от нашей команды под ваш бюджет и возможности' +
-				'\n\nПосле завершения видео нажмите кнопку ниже'
-			});
+		const stepHandlerforAnswerOne = new Composer<IBotContext>();
+		stepHandlerforAnswerOne.action('0k50k', ctx => {
+			ctx.session.question1 = 'от 0 до 50 000 тысяч рублей';
 			ctx.wizard.next();
 		});
-		stepHandlerforVideo.action('100500', ctx => {
-			ctx.reply('Загрузка...');
-			ctx.telegram.sendVideo(ctx.chat?.id, { source: './src/public/video/video2.MP4' }, {
-				reply_markup: {
-					inline_keyboard: [[{text: 'ПРОСМОТРЕНО✅', callback_data: 'watch1_complete'}]]
-				},
-				width: 720,
-				height: 1280,
-				caption: '☝️ Посмотрите скорее видео, Виталий простым языком расскажет:' +
-				'\n\n1️⃣ Секрет, который не вошел в эфир' +
-				'\n2️⃣ Какие проекты вы можете использовать, чтобы заработать на дропах (инфа из платного курса!)' +
-				'\n3️⃣ Как прямо сейчас получить инструкцию напрямую от нашей команды под ваш бюджет и возможности' +
-				'\n\nПосле завершения видео нажмите кнопку ниже'
-			});
+		stepHandlerforAnswerOne.action('50k100k', ctx => {
+			ctx.session.question1 = 'от 50 000 до 100 000 тысяч рублей';
 			ctx.wizard.next();
 		});
-		stepHandlerforVideo.action('5001000', ctx => {
-			ctx.reply('Загрузка...');
-			ctx.telegram.sendVideo(ctx.chat?.id, { source: './src/public/video/video3.MP4' }, {
-				reply_markup: {
-					inline_keyboard: [[{text: 'ПРОСМОТРЕНО✅', callback_data: 'watch1_complete'}]]
-				},
-				width: 720,
-				height: 1280,
-				caption: '☝️ Посмотрите скорее видео, Виталий простым языком расскажет:' +
-				'\n\n1️⃣ Секрет, который не вошел в эфир' +
-				'\n2️⃣ Какие проекты вы можете использовать, чтобы заработать на дропах (инфа из платного курса!)' +
-				'\n3️⃣ Как прямо сейчас получить инструкцию напрямую от нашей команды под ваш бюджет и возможности' +
-				'\n\nПосле завершения видео нажмите кнопку ниже'
-			});
+		stepHandlerforAnswerOne.action('100k+', ctx => {
+			ctx.session.question1 = 'от 100 000 тысяч рублей';
 			ctx.wizard.next();
 		});
 
 		const stepHandlerforAnswerTwo = new Composer<IBotContext>();
 		stepHandlerforAnswerTwo.action('0k50k', ctx => {
-			ctx.session.question1 = 'от 0 до 50 000 тысяч рублей';
+			ctx.session.question2 = 'от 0 до 50 000 тысяч рублей';
 			ctx.wizard.next();
 		});
 		stepHandlerforAnswerTwo.action('50k100k', ctx => {
-			ctx.session.question1 = 'от 50 000 до 100 000 тысяч рублей';
+			ctx.session.question2 = 'от 50 000 до 100 000 тысяч рублей';
 			ctx.wizard.next();
 		});
 		stepHandlerforAnswerTwo.action('100k+', ctx => {
-			ctx.session.question1 = 'от 100 000 тысяч рублей';
+			ctx.session.question2 = 'от 100 000 тысяч рублей';
 			ctx.wizard.next();
 		});
 
 		const stepHandlerforAnswerThree = new Composer<IBotContext>();
 		stepHandlerforAnswerThree.action('15min', ctx => {
-			ctx.session.question1 = 'до 15 минут в день';
+			ctx.session.question3 = 'до 15 минут в день';
 			ctx.wizard.next();
 		});
 		stepHandlerforAnswerThree.action('1hour', ctx => {
-			ctx.session.question1 = 'до часа в день';
+			ctx.session.question3 = 'до часа в день';
 			ctx.wizard.next();
 		});
 		stepHandlerforAnswerThree.action('1hour+', ctx => {
-			ctx.session.question1 = 'больше часа в день';
+			ctx.session.question3 = 'больше часа в день';
 			ctx.wizard.next();
 		});
 
 		this.scene = new Scenes.WizardScene(
 			'learn',
 			async ctx => {
-				ctx.replyWithHTML('Ваш урок уже грузится!\n\nА пока ответьте на вопрос: <b>Какую сумму вы собираетесь использовать для работы с дропами?</b>', {
+				ctx.reply('Загрузка информации по действующим дропам...')
+				ctx.telegram.sendVideo(ctx.chat?.id, { source: './src/public/video/greeting.MP4' }, greeting);
+			},
+			async ctx => {
+				ctx.replyWithHTML('Ответьте на вопрос: <b>Какую сумму вы собираетесь использовать для работы с дропами?</b>', {
 					reply_markup: {
 						inline_keyboard: [
 							[{ text: 'от 0 до 5 000 РУБ', callback_data: '0100'}],
@@ -101,9 +70,9 @@ export class LearnScene extends Scene {
 				}),
 				ctx.wizard.next();
 			},
-			stepHandlerforVideo,
+			stepHandlerforAnswerOne,
 			async ctx => {
-				ctx.replyWithHTML('Следующий урок уже грузится!\n\nА пока ответьте на вопрос: <b>Сколько вы хотите заработать на дропах?</b>', {
+				ctx.replyWithHTML('Ваш урок уже формируется!\n\nОтветьте на вопрос: <b>Сколько вы хотите заработать на дропах?</b>', {
 					reply_markup: {
 						inline_keyboard: [
 							[{ text: 'от 0 до 50 000 РУБ', callback_data: '0k50k'}],
@@ -116,19 +85,7 @@ export class LearnScene extends Scene {
 			},
 			stepHandlerforAnswerTwo,
 			async ctx => {
-				ctx.reply('Загрузка...');
-				ctx.telegram.sendVideo(ctx.chat?.id, { source: './src/public/video/video4.MP4' }, {
-					reply_markup: {
-						inline_keyboard: [[{text: 'ПРОСМОТРЕНО✅', callback_data: 'watch1_complete'}]]
-					},
-					width: 720,
-					height: 1280,
-					caption: '☝️ Посмотрите финальное видео',
-				});
-				ctx.wizard.next();
-			},
-			async ctx => {
-				ctx.replyWithHTML('Спасибо, что просмотрели уроки!\n\nНа основе полученной информации поделитесь пока ответьте на вопрос: <b>Сколько времени вы готовы уделять?</b>', {
+				ctx.replyWithHTML('Урок уже почти сформирован!\n\nОтветьте на вопрос: <b>Сколько времени вы готовы уделять дропам?</b>', {
 					reply_markup: {
 						inline_keyboard: [
 							[{ text: 'до 15 минут в день', callback_data: '15min'}],
@@ -141,7 +98,32 @@ export class LearnScene extends Scene {
 			},
 			stepHandlerforAnswerThree,
 			async ctx => {
-				ctx.reply('Спасибо, что уделили время. Для дополнительной консультации и записи на персональную консультацию звоните +79871011090');
+				switch (ctx.session.question1) {
+					case 'от 0 до 5 000 РУБ':
+						ctx.reply('Загрузка...');
+						ctx.telegram.sendVideo(ctx.chat?.id, { source: './src/public/video/video1.MP4' }, video0_5000);
+						ctx.wizard.next();
+						break;
+
+					case 'от 5 000 до 25 000 РУБ':
+						ctx.reply('Загрузка...');
+						ctx.telegram.sendVideo(ctx.chat?.id, { source: './src/public/video/video2.MP4' }, video5000_10000);
+						ctx.wizard.next();
+						break;
+
+					case 'от 25 000 РУБ':
+						ctx.reply('Загрузка...');
+						ctx.telegram.sendVideo(ctx.chat?.id, { source: './src/public/video/video3.MP4' }, video10000);
+						ctx.wizard.next();
+						break;
+				
+					default:
+						break;
+				}
+			},
+			async ctx => {
+				ctx.reply('Загрузка...');
+				ctx.telegram.sendVideo(ctx.chat?.id, { source: './src/public/video/final.MP4' }, final);
 				ctx.scene.leave();
 			}
 		);
