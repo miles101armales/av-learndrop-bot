e910c697ea766c7ce1a558d2a78a961cfc7df0ca
Arman Kuzembaev <armansk8er@gmail.com>
2024-05-16 04:02:58 +0500

    added exception filters

--- src/controllers/start.command.ts
+++ src/controllers/start.command.ts
@@ -1,21 +1,33 @@
+import { ExceptionFilter } from '../utils/error/exception.service';
 import { Bot } from '../app';
 import { Command } from '../models/command.class';
 import { ConfigService } from '../utils/config/config.service';
 import { LoggerService } from '../utils/logger/logger.service';
+import { Telegraf } from 'telegraf';
+import { IBotContext } from '../models/context.interface';
 
 export class StartCommand extends Command {
+	exceptionFilter: ExceptionFilter;
+	constructor(bot: Telegraf<IBotContext>) {
+		super(bot)
+		this.exceptionFilter = new ExceptionFilter()
+	}
 	handle(): void {
-		try {
-			this.bot.start((ctx) => {
-				ctx.sendMessage('Введите пожалуйста номер телефона, чтобы открыть доступ и далее получить информацию о дропах и инструкцию');
+		this.bot.start((ctx) => {
+			try {
+				ctx.sendMessage('Введите пожалуйста номер телефона, чтобы открыть доступ и далее получить информацию о дропах и инструкцию\n\nЕсли вы не получили ответа, проверьте корректность введеного вами номера');
 				this.bot.hears(/^\+?[0-9]{1,3}[ -]?\(?[0-9]{3}\)?[ -]?[0-9]{3}[ -]?[0-9]{2}[ -]?[0-9]{2}$/, (ctx) => {
-					ctx.session.phoneToCall = ctx.msg.text;
-					ctx.scene.enter('learn');
+					try {
+						ctx.session.phoneToCall = ctx.msg.text;
+						ctx.scene.enter('learn');
+					} catch (error) {
+						this.exceptionFilter.handle(error);
+					}
 				});
-			})
-		} catch (error) {
-			console.error(error)
-		}
+			} catch (error) {
+				this.exceptionFilter.handle(error);
+			}
+		})
 	}
 
 }
